da5492a1befa4dba47f80670e62e96cadc80c64d
hmc1985 <hmc19850203@163.com>
2018-07-16 17:43:01 +0800

    1111111111111

--- app/src/main/java/xposedtest/hmc/xposedtest/MainActivity.java
+++ app/src/main/java/xposedtest/hmc/xposedtest/MainActivity.java
@@ -1,16 +1,7 @@
 package xposedtest.hmc.xposedtest;
 
-import android.Manifest;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.net.wifi.WifiManager;
-import android.os.Build;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.telephony.TelephonyManager;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
